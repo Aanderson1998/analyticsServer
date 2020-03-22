45f530dc2edcbb75ad85d700539df05c245878a2
Aryanna Anderson <tuh05038@temple.edu>
2020-03-22 19:20:01 -0400

    Fixing cors issue so it can handle request from localhost8080 (part 1 and part 3 of assignment)

--- AnalyticsServer/src/main/java/com/analyticsServer/AnalyticsServer/trackViewServer.java
+++ AnalyticsServer/src/main/java/com/analyticsServer/AnalyticsServer/trackViewServer.java
@@ -7,13 +7,14 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class trackViewServer {
 
 	// in memory data structure to hold values every time user enters web site
--- AnalyticsServer/src/main/java/com/analyticsServer/AnalyticsServer/viewServer.java
+++ AnalyticsServer/src/main/java/com/analyticsServer/AnalyticsServer/viewServer.java
@@ -7,12 +7,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class viewServer {
 
 
