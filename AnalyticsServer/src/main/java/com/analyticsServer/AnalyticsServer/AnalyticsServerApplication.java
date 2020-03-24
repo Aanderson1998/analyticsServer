afcc730f1e250f0a00dfab75dffba43cfdba0f91
Aryanna Anderson <tuh05038@temple.edu>
2020-03-23 23:45:05 -0400

    adding in comments

--- AnalyticsServer/src/main/java/com/analyticsServer/AnalyticsServer/AnalyticsServerApplication.java
+++ AnalyticsServer/src/main/java/com/analyticsServer/AnalyticsServer/AnalyticsServerApplication.java
@@ -4,13 +4,11 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
-
+//class to run server 
 @SpringBootApplication
 public class AnalyticsServerApplication {
 
-	public static void main(String[] args)throws IOException {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(AnalyticsServerApplication.class, args);
 	}
 
